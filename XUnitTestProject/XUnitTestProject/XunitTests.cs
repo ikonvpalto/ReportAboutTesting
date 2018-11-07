e31a6eb5f5008b1d06700392c86ff85c65c583e5
Valeriy <konvpalto87@gmail.com>
2018-11-07 23:44:00 +0300

    Almost done with xUnit chapter

--- XUnitTestProject/XUnitTestProject/XunitTests.cs
+++ XUnitTestProject/XUnitTestProject/XunitTests.cs
@@ -1,8 +1,9 @@
 ﻿using Xunit;
+using System;
 
 namespace XUnitTestProject
 {
-    public class UnitTests
+    public class XunitTests
     {
         [Fact]
         public void TestSimple()
@@ -10,6 +11,33 @@ namespace XUnitTestProject
             int num = 1 + 2;
 
             Assert.Equal(3, num);
+        }
+
+        [Fact]
+        public void TestException()
+        {
+            string a = null;
+
+            NullReferenceException throwedException =
+                Assert.Throws<NullReferenceException>(() => a.Insert(1, "asd"));
+
+            Assert.Equal(nameof(XUnitTestProject), throwedException.Source);
+        }
+
+        public int q = 1;
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, int.MaxValue, int.MaxValue)]
+        [InlineData(int.MinValue, int.MaxValue, -1)]
+        [InlineData(int.MaxValue, int.MaxValue, -2)]
+        [InlineData(int.MinValue, int.MinValue, 0)]
+        public void TestTheorySimple(int a, int b, int expected)
+        {
+            int num = a + b;
+
+            Assert.Equal(expected, num);
         }
     }
 }
